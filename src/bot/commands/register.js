d86ad40de5038c8b86f400118bc553c384006c42
Yuu6883 <yuu6883dev@gmail.com>
2025-05-16 01:29:05 -0700

    remove unused commands

--- src/bot/commands/register.js
+++ src/bot/commands/register.js
@@ -53,67 +53,67 @@ const commands = [
             },
         ],
     },
-    {
-        name: 'link',
-        description: 'Link external account to your discord account in Allmind database',
-        options: [
-            {
-                type: 3,
-                name: 'provider',
-                description: 'Third party account provider',
-                required: true,
-                choices: [
-                    {
-                        name: 'Challonge',
-                        value: 'challonge',
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        name: 'challonge',
-        description: 'Connect a Challonge tournament to Discord',
-        options: [
-            {
-                type: 1,
-                name: 'setup',
-                description: 'Set up a Challonge tournament in this guild',
-                options: [
-                    {
-                        type: 3,
-                        name: 'tournament',
-                        description: 'Tournament ID on Challonge',
-                        required: true,
-                    },
-                    {
-                        type: 7,
-                        name: 'channel',
-                        description: 'Channel to host the tournament',
-                        required: true,
-                    },
-                    {
-                        type: 8,
-                        name: 'role',
-                        description: 'Moderation role for the tournament',
-                        required: true,
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        name: 'p2p',
-        description: 'Test p2p ping with a user',
-        options: [
-            {
-                type: 6,
-                name: 'user',
-                description: 'User to invite to a p2p ping test',
-                required: true,
-            },
-        ],
-    },
+    // {
+    //     name: 'link',
+    //     description: 'Link external account to your discord account in Allmind database',
+    //     options: [
+    //         {
+    //             type: 3,
+    //             name: 'provider',
+    //             description: 'Third party account provider',
+    //             required: true,
+    //             choices: [
+    //                 {
+    //                     name: 'Challonge',
+    //                     value: 'challonge',
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: 'challonge',
+    //     description: 'Connect a Challonge tournament to Discord',
+    //     options: [
+    //         {
+    //             type: 1,
+    //             name: 'setup',
+    //             description: 'Set up a Challonge tournament in this guild',
+    //             options: [
+    //                 {
+    //                     type: 3,
+    //                     name: 'tournament',
+    //                     description: 'Tournament ID on Challonge',
+    //                     required: true,
+    //                 },
+    //                 {
+    //                     type: 7,
+    //                     name: 'channel',
+    //                     description: 'Channel to host the tournament',
+    //                     required: true,
+    //                 },
+    //                 {
+    //                     type: 8,
+    //                     name: 'role',
+    //                     description: 'Moderation role for the tournament',
+    //                     required: true,
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // },
+    // {
+    //     name: 'p2p',
+    //     description: 'Test p2p ping with a user',
+    //     options: [
+    //         {
+    //             type: 6,
+    //             name: 'user',
+    //             description: 'User to invite to a p2p ping test',
+    //             required: true,
+    //         },
+    //     ],
+    // },
     {
         name: 'random',
         description: 'Generate random AC with specific constraints',
